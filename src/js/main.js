557e38cb0473781463058beb3e4f628f1c3f851d
dankvul <daniil.kuvaldin@icloud.com>
2022-05-20 09:57:37 +0300

    added index.html

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".top-slider__inner").slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        // autoplaySpeed: 1000,
+    });
+});
